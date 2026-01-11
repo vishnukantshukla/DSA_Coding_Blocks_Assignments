08cfb4d95e7b97414606ce5291f185c8e52d2299
vishnukantshukla <vishnukantshukla820@gmail.com>
2026-01-11 13:56:45 +0530

    Updated files

---
+++ Assignment2/Print_Series.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Print_Series {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        for(int i=1;i<=n1;i++){
+            int temp = 3*i + 2;
+            if(temp%n2!=0){
+                System.out.println(temp);
+            }
+            else{
+                n1++;
+            }
+        }
+
+    }
+}
